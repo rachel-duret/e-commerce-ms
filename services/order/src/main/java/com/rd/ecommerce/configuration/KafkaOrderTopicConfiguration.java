bd5cf901835fa1b22f47b6f7f57969f97a990582
rachel <chuncheung.ku@gmail.com>
2025-10-07 11:41:51 +0200

    add kafka and zookeeper config

---
+++ services/order/src/main/java/com/rd/ecommerce/configuration/KafkaOrderTopicConfiguration.java
@@ -0,0 +1,15 @@
+package com.rd.ecommerce.configuration;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaOrderTopicConfiguration {
+    @Bean
+    public NewTopic orderTopic() {
+        return TopicBuilder.name("order-topic").build();
+    }
+}
