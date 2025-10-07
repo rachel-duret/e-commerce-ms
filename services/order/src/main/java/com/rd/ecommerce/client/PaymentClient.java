9ac01dbecdae76848b568daaffd2ec5d1f693109
rachel <chuncheung.ku@gmail.com>
2025-10-07 14:02:59 +0200

    init payment service

---
+++ services/order/src/main/java/com/rd/ecommerce/client/PaymentClient.java
@@ -0,0 +1,16 @@
+package com.rd.ecommerce.client;
+
+
+import com.rd.ecommerce.dto.PaymentRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "payment-service",
+        url = "${application.config.paymentUrl}"
+)
+public interface PaymentClient {
+    @PostMapping("/payment")
+    Integer requestOrderPayment(@RequestBody PaymentRequest request);
+}
---
+++ services/order/src/main/java/com/rd/ecommerce/dto/PaymentRequest.java
@@ -0,0 +1,14 @@
+package com.rd.ecommerce.dto;
+
+import com.rd.ecommerce.models.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        Integer orderId,
+        String orderReference,
+        CustomerResponse customer
+) {
+}
